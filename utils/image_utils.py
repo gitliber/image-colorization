aa6bcdd9e2570d5a67e1055153381dc3896a5aff
Jair Ribeiro <41255414+liberoliber@users.noreply.github.com>
2020-06-25 09:50:33 +0200

    Add files via upload

---
+++ utils/image_utils.py
@@ -0,0 +1,102 @@
+import os
+import urllib.request
+
+import scipy.misc
+from PIL import Image
+import numpy as np
+from progressbar import ProgressBar, Percentage, Bar
+from skimage import color
+
+
+def load_images(file):
+    """
+    Function loads black and white image and convert it to CIE LAB
+
+    Parameters
+    ----------
+    file : str
+        Path to image file
+
+    Returns
+    -------
+    ndarray
+        3D array with image
+    """
+
+    try:
+        img = Image.open(file)
+    except (OSError, ValueError, IOError, ZeroDivisionError) as e:
+        print("Can not open file", file, "Error: ", e)
+        return None
+    img = img.convert(mode="RGB")  # ensure that image rgb
+    rgb = np.array(img)
+
+    return color.rgb2lab(rgb)
+
+
+def resize_image_lab(im, size):
+    """
+    This function resizes images of any colorspace and number of channels
+
+    Parameters
+    ----------
+    im : ndarray
+        Numpy array that contains image
+    size : (int, int)
+        New size of image
+
+    Returns
+    -------
+    ndarray
+        Resized image
+    """
+    # because there is not good resizer for a LAB images
+    im = (color.lab2rgb(im) * 255).astype(int)
+    img = scipy.misc.imresize(im, size)
+
+    from skimage.transform import resize
+    #img = resize(im, output_shape=(size))
+
+
+
+
+    return color.rgb2lab(np.array(img))
+
+
+def get_weights(file_name):
+    """
+    Function check if weights exist. If they not it downloads them from release in Github.
+
+    Parameters
+    ----------
+    file_name : str
+        Path to the weight file or its destination to download
+
+    Returns
+    -------
+    str
+        Path to the weight file
+    """
+    weights_url = "https://github.com/PrimozGodec/ImageColorization/releases/download/v0.0.1"
+
+    # if file do not exist download it
+    print(file_name)
+    if not os.path.isfile(file_name):
+        print("Downloading trained model")
+        # init progress bar
+        pbar = None
+
+        def show_progress(count, block_size, total_size):
+            nonlocal pbar
+            if pbar is None:
+                pbar = ProgressBar(maxval=total_size, widgets=[Percentage(), Bar()])
+                pbar.start()
+            pbar.update(min(count * block_size, total_size))  # min because last block not full
+
+        # download
+        urllib.request.urlretrieve(os.path.join(weights_url, file_name.split("/")[-1]),
+                                   file_name,
+                                   reporthook=show_progress)
+        pbar.finish()
+        print("Downloading done")
+    return file_name
